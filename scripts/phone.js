114a03e133f5e210b97cfa11dab2c603ee3971ca
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-08-28 05:59:13 +0600

    basic card added

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,7 +1,41 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const data = await res.json();
-    console.log(data);
-}
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhones(phones);
+};
+
+const displayPhones = (phones) => {
+  // console.log(phones);
+  // 1 get the div
+  const phoneContainer = document.getElementById("phone-container");
+
+  phones.forEach((phone) => {
+    console.log(phone);
+    // 2 create a div element
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+    //3 set inner html
+    phoneCard.innerHTML = `
+        <figure>
+        <img
+          src="${phone.image}"
+          alt="iphone"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+        `;
+    // 4. append the phoneCard in the phoneContainer
+    phoneContainer.appendChild(phoneCard);
+  });
+};
 
 loadPhone();
