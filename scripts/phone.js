0f7a0f0f1d4ce7ca9933a7642ac4b0f0c391f2a6
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-08-29 07:50:01 +0600

    show all button fixed

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,41 +1,41 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
-  displayPhones(phones);
+  displayPhones(phones, isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isShowAll) => {
   // console.log(phones);
   // 1 get the div
   const phoneContainer = document.getElementById("phone-container");
-  // clear the container after a search
-//   phoneContainer.textContent = '';
+  // show all containers
+  const showAllContainer = document.getElementById("show-all-container");
 
-    // show all containers
-    const showAllContainer = document.getElementById('show-all-container');
-    if(phones.length > 12){
-        showAllContainer.classList.remove('hidden');
-    }else{
-        showAllContainer.classList.add('hidden');
-    }
-  // display only first 10 phones
-  phones = phones.slice(0, 12);
+  if (phones.length > 12 && !isShowAll) {
+    showAllContainer.classList.remove('hidden');
+  } else {
+    showAllContainer.classList.add('hidden');
+  }
+
+  console.log("is show all: ", isShowAll);
+
+  if (!isShowAll) {
+    // display only first 12 phones
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     // 2 create a div element
     const phoneCard = document.createElement("div");
     phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
     //3 set inner html
     phoneCard.innerHTML = `
-        <figure>
-        <img
-          src="${phone.image}"
-          alt="iphone"
-        />
+      <figure>
+        <img src="${phone.image}" alt="iphone" />
       </figure>
       <div class="card-body">
         <h2 class="card-title">${phone.phone_name}</h2>
@@ -48,15 +48,31 @@ const displayPhones = (phones) => {
     // 4. append the phoneCard in the phoneContainer
     phoneContainer.appendChild(phoneCard);
   });
+  // hide loading spinner
+  toggleLoadingSpinner(false);
 };
 
 // handle search button
-const handleSearch = () =>{
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    searchField.value = '';
-    console.log(searchText);
-    loadPhone(searchText)
-}
+const handleSearch = (isShowAll) => {
+  toggleLoadingSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  // searchField.value = "";
+  // console.log(searchText);
+  loadPhone(searchText, isShowAll);
+};
 
-// loadPhone();
+// loading spinner
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
+
+// handle show all
+const handleShowAll = () => {
+  handleSearch(true);
+};
